4fa3e22ca0b5201428db6e8672584547b2deea7f
mahadi-init <init.mahadi@gmail.com>
2025-07-30 08:18:30 +0600

    Remove: unncessary files and components

--- src/controller/admin.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import mongoose from "mongoose";
-import { SharedRequest } from "../helpers/SharedRequest";
-
-export class AdminRequest extends SharedRequest {
-  constructor(model: typeof mongoose.Model) {
-    super(model);
-  }
-}
--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import auth from "./../routes/auth.route";
-import admin from "./../routes/admin.route";
 
 const router = Router();
 
 router.use("/auth", auth);
-router.use("/admin", admin);
 
 // Handle not found
 router.use((req, res, next) => {
