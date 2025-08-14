236c463b6329c8a401bd5bb6592fc15293e72ee7
mahadi-init <init.mahadi@gmail.com>
2025-08-14 16:02:18 +0600

    allow all origin

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "*",
       "http://localhost:3000",
       "http://localhost:5173",
       "https://expense-tracker-two-wheat.vercel.app",
